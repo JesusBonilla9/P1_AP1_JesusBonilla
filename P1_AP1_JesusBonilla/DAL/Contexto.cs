63f57732e68b492a36fcc334154e0ab612d10560
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-09-22 20:40:45 -0400

    Se creo el Contexto

---
+++ P1_AP1_JesusBonilla/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_JesusBonilla.Models;
+namespace P1_AP1_JesusBonilla.DAL
+{
+    public class Contexto : DbContext
+    {
+        Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        DbSet<Registro> Registros { get; set; }
+    }
+}
